57215a8d7dcd60860598d83ed06ac2d9d487d4c1
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2023-10-19 11:11:22 +0000

    Complete Leetcode Even Number of Digits problem set.

---
+++ Leetcode/Data Structures/Arrays and Strings/Even Number of Digits/evenDigits.py
@@ -0,0 +1,39 @@
+'''
+    Given an array nums of integers, return how many of them contain an even number of digits.
+    
+    Example 1
+    Input: nums = [12,345,2,6,7896]
+        Output: 2
+        Explanation: 
+        12 contains 2 digits (even number of digits). 
+        345 contains 3 digits (odd number of digits). 
+        2 contains 1 digit (odd number of digits). 
+        6 contains 1 digit (odd number of digits). 
+        7896 contains 4 digits (even number of digits). 
+        Therefore only 12 and 7896 contain an even number of digits.
+        
+    Example 2
+    Input: nums = [555,901,482,1771]
+        Output: 1 
+        Explanation: 
+        Only 1771 contains an even number of digits.
+        
+    Constraints:
+        1 <= nums.length <= 500
+        1 <= nums[i] <= 105
+'''
+
+class Solution(object):
+    def findNumbers(self, nums):
+        evenNumberOfDigits = 0
+        for num in nums:
+            if len(str(num)) % 2 == 0:
+                evenNumberOfDigits += 1
+        return evenNumberOfDigits
+        
+def main():
+    solution = Solution()
+    print(solution.findNumbers([12,345,2,6,7896]))
+        
+if __name__ == '__main__':
+    main()
