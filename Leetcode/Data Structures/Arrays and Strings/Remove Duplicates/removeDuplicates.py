f8005965e3a84def7d2496113c943c4a1e1ced91
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-02-15 17:59:14 +0000

    Complete Better Repeated Deletion Algorithm.

--- Leetcode/Data Structures/Arrays and Strings/Remove Duplicates/removeDuplicates.py
+++ Leetcode/Data Structures/Arrays and Strings/Remove Duplicates/removeDuplicates.py
@@ -13,12 +13,20 @@
 
 class Solution():
     def removeDuplicates(self, nums):
-        writeReader = 0
-        
+        writeReader = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[writeReader] = nums[i]
+                writeReader += 1
+            print(nums)
+        print(writeReader)
+        return nums 
+
 
 def main():
     solution = Solution()
-    print(solution.removeDuplicates())
+    print(solution.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
 
 
 if __name__ == '__main__':
