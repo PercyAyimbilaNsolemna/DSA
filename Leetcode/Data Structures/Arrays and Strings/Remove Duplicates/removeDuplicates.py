92154c63341d61580bbcbb3b0ba7580f370c7589
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-02-15 17:47:32 +0000

    Start better repeated deletion algorithm.

---
+++ Leetcode/Data Structures/Arrays and Strings/Remove Duplicates/removeDuplicates.py
@@ -0,0 +1,25 @@
+''''
+    A Better Repeated Deletion Algorithm 
+
+    Anyway, the algorithm with O(N) space is surprisingly similar to the one without. Interestingly, it's simpler though, 
+    because it doesn't need to firstly determine the size of the output.
+
+    Implementing this requires the use of the two-pointer technique. This is where we iterate over the Array in two 
+    different places at the same time.
+
+    Read all the elements like we did before, to identify the duplicates. We call this our readPointer.
+    Keep track of the next position in the front to write the next unique element we've found. We call this our writePointer.
+'''
+
+class Solution():
+    def removeDuplicates(self, nums):
+        writeReader = 0
+        
+
+def main():
+    solution = Solution()
+    print(solution.removeDuplicates())
+
+
+if __name__ == '__main__':
+    main()
