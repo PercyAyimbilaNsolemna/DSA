b33061978aadd4f11f16c8a1767d9d9eb6984742
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-05-16 15:08:18 +0000

    Start implementing Matrix Data Structure.

---
+++ Data Structures/Matrix/matrix.py
@@ -0,0 +1,28 @@
+'''
+    Implementation of the matrix data structure
+
+    With the below method for a matrix opeartion
+
+    insert (item, i, j): Inserts an item at the given position
+    traverse(item): Print out every cell value of a matrix
+    search(item): Return true or false if an item can be found in a matrix
+    getIndex(item): Returns the idex i and j of an item in a matrix
+    reverseRow(rowIndex): Reverse elements of a specific row
+    reverseColumn(columnIndex): Reverse elements of a specific column
+    sort(item): Remove node containg a specified item
+    diagonalSum(): Returns the sum of cells along the matrix primary diagonal
+    display(): Print out every cell data in a matrix
+
+'''
+
+class Matrix:
+    def __init__(matrix=None):
+        ...
+
+
+def main():
+    matrix = Matrix()
+
+
+if __name__ == '__main__':
+    main()
