e1954df1c493cfa09eca6aed29577657cc2f720b
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-08 20:30:32 +0000

    Add a method that adds an item to the stack.

--- Data Structures/Stacks/Array Stack/arrayStack.py
+++ Data Structures/Stacks/Array Stack/arrayStack.py
@@ -11,12 +11,26 @@
 
 class Stack:
     #Method called when instantiating a stack class
-    def __init__(self, size=None):
+    def __init__(self, stack=[], size=0):
+        self.stack = stack
         #Creates a size property and set it to none when the Strack class is instantiated
         self.size = size
 
+    #Creates a method that adds an item to the stack
+    def append(self, item):
+        #Adds one to the size of the stack anytime an item is added so we can keep track of the size of the stack
+        self.size += 1
+        #Adds item to the end of the stack
+        self.stack.append(item)
+        return self.stack
+
 def main():
     stack = Stack()
+    print(stack.append(2))
+    print(stack.append(5))
+    print(stack.append(6))
+    print(stack.append(1))
+    print(stack.append(3))
     print(stack.size)
 
 if __name__ == '__main__':
