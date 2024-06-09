6057f7782e5d6a2005f47f350d6ec1dc34ff7d6d
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-09 19:42:48 +0000

    Add a method that outputs the last element in the stack.

--- Data Structures/Stacks/Array Stack/arrayStack.py
+++ Data Structures/Stacks/Array Stack/arrayStack.py
@@ -28,7 +28,7 @@ class Stack:
     def pop(self):
         #Checks if the stack is empty
         if self.size == 0:
-            raise Exception('Stack is empty')
+            raise Exception('Stack is EMPTY!')
         
         #Decreases the size of the stack by 1
         self.size -= 1
@@ -38,17 +38,45 @@ class Stack:
         
         #Retruns
         return
+    
+    #Creates a method that shows the last element in the statck without removing it
+    def peek(self):
+        #Checks if the stack is empty
+        if self.size == 0:
+            print('The stack is empty')
+
+            return 
+        
+        #Prints the last element in the stack
+        print(self.stack[self.size - 1])
+
+        return
 
 def main():
+    #Creates an object from the Stack class
     stack = Stack()
+
+    #Appends elements to the end of the stack
     print(stack.append(2))
     print(stack.append(5))
     print(stack.append(6))
     print(stack.append(1))
     print(stack.append(3))
+
+    #Checks the size of the stack
     print(stack.size)
+
+    #Removes an element from the end of the stack (LIFO)
     stack.pop()
     print(stack.stack)
+
+    #Gets the last element in the stack
+    stack.peek()
+
+    stack1 = Stack()
+    stack1.peek()
+    #Shoots an error since the stack is empty
+    stack1.pop()
 
 if __name__ == '__main__':
     main()
