bd69a32fc7855611cc0bccaa5d4022c5b53e35e5
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-10 20:29:03 +0000

    Start implementing stack using linked list.

---
+++ Data Structures/Stacks/LinkedList Stack/linkedListStack.py
@@ -0,0 +1,30 @@
+'''
+ Implementation of Stack in python using Linked List
+
+    Operations: append, pop, peek, isEmpty
+
+    With their respective time and space complexity
+
+    Can't wait to review the code with you
+
+'''
+#Creates a class that creates a node in the linked list
+class Node:
+    def __init__(self, data, ref=None):
+        #Sets the data and the ref to the parameter passed to the Node class
+        self.data = data
+        self.ref = ref
+
+        
+#Creates a class that creates the linked list stack
+class Stack:
+    def __init__(self):
+        ...
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
