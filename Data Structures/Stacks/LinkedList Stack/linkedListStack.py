48c35727df292bbc348f53575e651e9a14c0ef95
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-11 13:40:13 +0000

    Add a method that outputs the size of the stack.

--- Data Structures/Stacks/LinkedList Stack/linkedListStack.py
+++ Data Structures/Stacks/LinkedList Stack/linkedListStack.py
@@ -12,17 +12,17 @@
 class Node:
     def __init__(self, data, ref=None):
         #Sets the data and the ref to the parameter passed to the Node class
-        self.data = data
-        self.ref = ref
+        self._data = data
+        self._ref = ref
 
         
 #Creates a class that creates the linked list stack
 class Stack:
     def __init__(self):
         #Sets the head of the linked list to None durimg instantiation
-        self.head = None
+        self._head = None
         #Sets the size of the stack to zero
-        self.size = 0
+        self._size = 0
 
     #Creates a method that adds an item to the end of the stack
     #Time complexity O(1) Constant time
@@ -30,63 +30,69 @@ class Stack:
         #Creates a new node
         newNode = Node(item)
         #Sets the ref of the newly created head to the previous head, if the first node then the ref is set to none
-        newNode.ref = self.head
+        newNode._ref = self._head
         #Sets the newNode as the head of the linked list
-        self.head = newNode
+        self._head = newNode
         #Adds one to the size of the stack
-        self.size += 1
+        self._size += 1
         return
 
     #Creates a method that remmoves an element from the end of a stack
     #Time complexity O(1) Constant time
     def pop(self):
         #Checks if the stack is not empty
-        if self.size == 0:
+        if self._size == 0:
             print('The stack is EMPTY!')
             return
         #Sets the head of the stack to the ref of the current stack
-        self.head = self.head.ref
+        self._head = self._head._ref
 
         #Decrease the size of the stack by 1
-        self.size -= 1
+        self._size -= 1
         return
     
     #Creates a method that outputs the top of the stack
     #Time complexity O(1) Constant time
     def peek(self):
         #Checks if the stack is empty
-        if self.size == 0:
+        if self._size == 0:
             print('The stack is EMPTY!')
             return 
-        print(self.head.data)
+        print(self._head._data)
 
     #Creates a method that checks if the stack is empty
     #Time complexity O(1) Constant Time
     def isEmpty(self):
         #Checks if the size of the stack is 0
-        if self.size == 0:
+        if self._size == 0:
             print(True)
             return
         
         #Outputs False if the stack has got elements
         print(False)
         return
+    
+    #Creates a method that outputs the size of the stack
+    #Time complexity O(1) Constant Time
+    def size(self):
+        print(self._size)
+        return
 
     #Creates a method that outputs all the elements in the stack
     def traverse(self):
         #Checks if the head of the linked list is empty then it outputs the linked list is empty
-        if self.head is None:
+        if self._head is None:
             print('Linked List is empty')
             return
         else:
             #Pulls the head of the linked list
-            n = self.head
+            n = self._head
             
             #Enters the loop if the head is not none and the folloowing nodes are having a ref to a different node 
             while n is not None:
-                print(n.data, '===>', end=' ')
+                print(n._data, '===>', end=' ')
                 #Sets the n to the ref of the next node
-                n = n.ref
+                n = n._ref
         print()
         return
 
@@ -109,7 +115,7 @@ def main():
 
     stack.traverse()
 
-    print(stack.size)
+    stack.size()
 
     stack.peek()
     stack.isEmpty()
