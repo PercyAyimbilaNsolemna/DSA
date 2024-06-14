13fa606feb7d0d14edacb050d98e5b419c4991c0
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-14 13:39:49 +0000

    Add traverse method that outputs each of the nodes in the queue to the console.

--- Data Structures/Queues/Linked List Queue/queue.py
+++ Data Structures/Queues/Linked List Queue/queue.py
@@ -21,6 +21,22 @@ class Queue():
         #Sets the head of the queue to none
         self.head = None
 
+    #Creates a traverse method that loops through the queue and outputs each node in the queue
+    def traverse(self):
+        #Checks if the head of the linked list is empty then it outputs the linked list is empty
+        if self.head is None:
+            print('Linked List is empty')
+            return
+        else:
+            #Pulls the head of the linked list
+            n = self.head
+            
+            #Enters the loop if the head is not none and the folloowing nodes are having a ref to a different node 
+            while n is not None:
+                print(n.data, '===>', end=' ')
+                #Sets the n to the ref of the next node
+                n = n.ref
+            return
 
 def main():
     ...
