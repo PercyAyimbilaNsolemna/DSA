67b88ae52af495feff7fb0ca787698e281d0e0c0
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-14 14:28:11 +0000

    Update the names of the attributes in the queue class.

--- Data Structures/Queues/Linked List Queue/queue.py
+++ Data Structures/Queues/Linked List Queue/queue.py
@@ -12,18 +12,18 @@
 #Creates a class node which will be used to create nodes in the linked lst
 class Node():
     def __init__(self, data, ref=None):
-        self.data = data
-        self.ref = ref
+        self._data = data
+        self._ref = ref
 
 #Creates a class to be used to create the queue
 class Queue():
     def __init__(self):
         #Sets the head of the queue to none
-        self.head = None
+        self._head = None
         #Sets the tail to none
-        self.tail = None
+        self._tail = None
         #Sets the size of the queue to zero
-        self.size = 0
+        self._size = 0
 
 
     #Creates a class that adds an element to the queue
@@ -32,52 +32,52 @@ class Queue():
         newNode = Node(item)
 
         #Checks if the queue is empty
-        if self.size == 0:
+        if self._size == 0:
             #Sets the ref of the newNode to None
-            newNode.ref = self.head
+            newNode._ref = self._head
             #Sets the newNode as the head node
-            self.head = newNode
+            self._head = newNode
             #Sets the newNode as the tail 
-            self.tail = self.head
-            self.size += 1
+            self._tail = self._head
+            self._size += 1
             return
         
         #If there is/are nodes in the queue
         #Creates a newNode
         newNode = Node(item)
         #Sets the ref of the last node in the queue to the new node added
-        self.tail.ref = newNode
+        self._tail._ref = newNode
         #Sets the tail of the queue to the new node
-        self.tail = newNode
+        self._tail = newNode
         #Increment the size of the queue by 1
-        self.size += 1
+        self._size += 1
         return
     
     #Creates a method that removes the first item from the queue
     def dequeue(self):
         #Checks if the queue is empty
-        if self.size == 0:
+        if self._size == 0:
             raise Exception('The queue is EMPTY!')
         
         #Removes the first element from the queue by setting the head to the ref of the current head
-        self.head = self.head.ref
-        self.size -= 1
+        self._head = self._head._ref
+        self._size -= 1
 
     #Creates a traverse method that loops through the queue and outputs each node in the queue
     def traverse(self):
         #Checks if the head of the linked list is empty then it outputs the linked list is empty
-        if self.head is None:
+        if self._head is None:
             print('Linked List is empty')
             return
         else:
             #Pulls the head of the linked list
-            n = self.head
+            n = self._head
             
             #Enters the loop if the head is not none and the folloowing nodes are having a ref to a different node 
             while n is not None:
-                print(n.data, '===>', end=' ')
+                print(n._data, '===>', end=' ')
                 #Sets the n to the ref of the next node
-                n = n.ref
+                n = n._ref
             print()
             return
 
@@ -88,7 +88,7 @@ def main():
     queue.enqueue(5)
     queue.enqueue(6)
     queue.enqueue(7)
-    print(queue.size)
+    #print(queue.size)
     queue.traverse()
     queue.dequeue()
     queue.traverse()
