f64606a01fb889af112de68a6cbd221475089fba
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-14 14:09:43 +0000

    Add an enqueue method that adds an item to the queue.

--- Data Structures/Queues/Linked List Queue/queue.py
+++ Data Structures/Queues/Linked List Queue/queue.py
@@ -20,6 +20,38 @@ class Queue():
     def __init__(self):
         #Sets the head of the queue to none
         self.head = None
+        #Sets the tail to none
+        self.tail = None
+        #Sets the size of the queue to zero
+        self.size = 0
+
+
+    #Creates a class that adds an element to the queue
+    def enqueue(self, item):
+        #Creates a new node
+        newNode = Node(item)
+
+        #Checks if the queue is empty
+        if self.size == 0:
+            #Sets the ref of the newNode to None
+            newNode.ref = self.head
+            #Sets the newNode as the head node
+            self.head = newNode
+            #Sets the newNode as the tail 
+            self.tail = self.head
+            self.size += 1
+            return
+        
+        #If there is/are nodes in the queue
+        #Creates a newNode
+        newNode = Node(item)
+        #Sets the ref of the last node in the queue to the new node added
+        self.tail.ref = newNode
+        #Sets the tail of the queue to the new node
+        self.tail = newNode
+        #Increment the size of the queue by 1
+        self.size += 1
+        return
 
     #Creates a traverse method that loops through the queue and outputs each node in the queue
     def traverse(self):
@@ -36,10 +68,18 @@ class Queue():
                 print(n.data, '===>', end=' ')
                 #Sets the n to the ref of the next node
                 n = n.ref
+            print()
             return
 
 def main():
-    ...
+    queue = Queue()
+
+    queue.enqueue(4)
+    queue.enqueue(5)
+    queue.enqueue(6)
+    queue.enqueue(7)
+    print(queue.size)
+    queue.traverse()
 
 
 if __name__ == '__main__':
