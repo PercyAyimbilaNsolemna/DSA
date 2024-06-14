e46e9dcbcf7d1bc570ee8285375f50deaed3b2db
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-14 13:35:01 +0000

    Start queue implementation using linked list.

---
+++ Data Structures/Queues/Linked List Queue/queue.py
@@ -0,0 +1,30 @@
+''''
+    Implementation of Queues in python using Linked List
+
+    Operations: enqueue, dequeue, top, and size
+
+    With their respective time and space complexity
+
+    Be my guest
+
+'''
+
+#Creates a class node which will be used to create nodes in the linked lst
+class Node():
+    def __init__(self, data, ref=None):
+        self.data = data
+        self.ref = ref
+
+#Creates a class to be used to create the queue
+class Queue():
+    def __init__(self):
+        #Sets the head of the queue to none
+        self.head = None
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
