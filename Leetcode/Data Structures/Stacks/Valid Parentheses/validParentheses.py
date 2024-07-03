e4971e1bd2f04ad64930e10a2807a96ce4c11af1
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-07-03 18:51:02 +0000

    Complete Leetcode Valid Parentheses problem set.

--- Leetcode/Data Structures/Stacks/Valid Parentheses/validParentheses.py
+++ Leetcode/Data Structures/Stacks/Valid Parentheses/validParentheses.py
@@ -37,11 +37,41 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        ...
+        '''
+        Algorithm
+
+        Loop through s and check if the top of the stack is the same as the same char then pop the char from thye stack
+        If not push the char to the stack 
+
+        '''
+        #Creates a hash map that saves each parenthses and the opposite
+        mapping = {'(': ')', '{': '}', '[': ']'}
+
+        #Creates an empty stack
+        stack = []
+
+        #Loops through the string(s)
+        for char in s:
+            #Checks if the stack is not empty and the char at the top of the stack is the same as the current char
+            if stack and mapping.get(stack[-1]) == char:
+                #Reomves the char from the top of the stack
+                stack.pop()
+            #If the stack is not empty and the top char is not ythe same as the char, adds the char to the top of the stack
+            else:
+                stack.append(char)
+
+        #Checks if the stack is not empty then it returns False
+        if stack :
+            return False
+        
+        #If not it returns True since the stack is empty
+        return True
 
 
 def main():
-    ...
+    solution = Solution()
+
+    print(solution.isValid('()[]{}'))
 
 
 
