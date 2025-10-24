e59f4ab86602c37c9df11cb9b598ed159c1b1a54
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2025-10-24 12:21:07 +0000

    Complete Remove nth Node from the End of List.

---
+++ Grokking the coding Interview/Patterns/Two Pointers/removeNode.py
@@ -0,0 +1,66 @@
+'''
+    Link to problem: https://www.educative.io/interview-prep/coding/remove-nth-node-from-end-of-list
+
+    Problem: Remove nth Node from End of List
+    Takes 30 mins
+
+    Statement
+    Given the head of a singly linked list and an integer n, remove the nth node from the end of the list and return 
+    the head of the modified list.
+
+    Constraints:
+
+    The number of nodes in the list is k
+    
+    1 ≤ k ≤ 10³
+
+    -10³ ≤ Node.Value ≤ 10³
+
+    1 ≤ n ≤ k
+
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+'''
+
+#Definition for a Linked List node
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+#Might 
+#from ds_v1.LinkedList.LinkedList import ListNode
+
+def remove_nth_last_node(head, n):
+    #Creates a dummy node to fix incorrect indexes
+    dummy = ListNode(val=0)
+    #Sets the head node to dummy
+    dummy.next = head
+    
+    #Initialize the first and second pointers and initialze them to the head
+    firstPointer = dummy
+    secondPointer = dummy
+    
+    #Moves the secondPointer n steps ahead of the firstPointer
+    for _ in range(n):
+        secondPointer = secondPointer.next
+        
+    #Moves the firstPointer and secondPointer until the secondPointer hits the last node
+    while secondPointer.next != None:
+        firstPointer = firstPointer.next
+        secondPointer = secondPointer.next
+        
+    #Changes the current pointer of the firstPointer to the next two addresses 
+    firstPointer.next = firstPointer.next.next
+    
+    #Eliminates the dummy and return the actual head of the linked list
+    return dummy.next
+
+def main():
+    #Couldn't test this becasue python has no built in linked list data structure but will work on it in the near future
+    #Java and C++ have built in linked list they can be used to test the code otherwise use leetcode or grokking the coding interview
+
+    ...
+
+if __name__ == '__main__':
+    main()
