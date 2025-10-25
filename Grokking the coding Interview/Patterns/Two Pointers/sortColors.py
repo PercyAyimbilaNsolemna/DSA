ce5dca09ae29c25c55e25964fa08dfbb94405747
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2025-10-25 00:46:16 +0000

    Add the problem statement, the link to the problem set, and the time and space complexity.

--- Grokking the coding Interview/Patterns/Two Pointers/sortColors.py
+++ Grokking the coding Interview/Patterns/Two Pointers/sortColors.py
@@ -1,6 +1,35 @@
 '''
-    Link: 
+    Link: https://www.educative.io/interview-prep/coding/sort-colors
 
+    Problem: Sort Colors
+    Takes 30 mins
+
+    Statement
+    Given an array, colors, which contains a combination of the following three elements:
+
+    0 (Representing red)
+
+    1 (Representing white)
+
+    2 (Representing blue)
+
+    Sort the array in place so that the elements of the same color are adjacent, and the final order is: 
+    red (0), then white (1), and then blue (2).
+
+    Note: You are not allowed to use any built-in sorting functions. The goal is to solve this efficiently without 
+    extra space.
+
+    Constraints:
+
+    n == colors.length
+
+    1 ≤ n ≤ 300
+
+    colors[i] is either 0, 1, or 2
+
+    Time Complexity: O(n)
+
+    Space Complexity: O(n)
 
 '''
 
